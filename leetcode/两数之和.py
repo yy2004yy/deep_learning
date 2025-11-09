ae20170dd6415706a105a3d61b65193c67381980
yangye <yangye659@gmail.com>
2025-11-09 18:08:14 +0800

    leetcode

---
+++ leetcode/两数之和.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i] + nums[j] == target):
+                    return [i,j]
+        return []
+        
+sol = Solution()
+nums = [2,5,7,8,9,15]
+target = 23
+print(sol.twoSum(nums, target))
